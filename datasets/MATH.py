8f3ef79d389147cffdbefe8473f4a9f3d463d885
Ting-Jun Wang <levi900227@gmail.com>
2024-05-02 16:46:08 +0800

    feat: sample 10 problems from each categories

--- datasets/MATH.py
+++ datasets/MATH.py
@@ -11,7 +11,7 @@ from utils.file import load_json
 import random
 
 class MATHDataset(Dataset):
-    def __init__(self, basedir, dataset_type='train', shuffle=True, problem_types=None):
+    def __init__(self, basedir, dataset_type='train', sample_per_categorie_num=10, problem_types=None):
         self.data = []
 
         for problem_type in os.listdir(f'{basedir}/{dataset_type}/'):
@@ -20,16 +20,16 @@ class MATHDataset(Dataset):
                     problem = load_json(f'{basedir}/{dataset_type}/{problem_type}/{problem_json}')
                     answer = self.extract_answer(problem['solution'])
 
-
                     if isinstance(answer, int):
                         self.data.append({
-                            'problem': problem['problem'],
-                            'level': problem['level'].replace('Level ', ''),
-                            'type': problem['type'],
-                            'answer': answer,
+                            'id': str(int(problem_json.replace('.json', ''))),
+                            'problem': str(problem['problem']),
+                            'level': str(problem['level'].replace('Level ', '')),
+                            'type': str(problem['type']),
+                            'answer': int(answer),
                         })
-        if shuffle:
-            random.shuffle(self.data)
+        random.shuffle(self.data)
+        self.sample_per_categories(sample_per_categorie_num)
 
     def __len__(self):
         return len(self.data)
@@ -46,8 +46,20 @@ class MATHDataset(Dataset):
         if match:
             return int(match.group(1))
         else:
-            #print("answer in boxed not found")
             return None
+
+    def sample_per_categories(self, num):
+        new_data = []
+        counter = {}
+        for i in self.data:
+            counter[i['type']] = 1 if i['type'] not in counter else counter[i['type']] + 1
+            if counter[i['type']] <= num:
+                new_data.append(i)
+        self.data = new_data
+
+    def get_data(self):
+        return self.data
+    
     
 def accuracy(model_output, actual, n_samples):
     '''
