cee0a9d69e3976b4e0198ceafe10ebcb3a5152a8
Ting-Jun Wang <levi900227@gmail.com>
2024-05-10 00:10:46 +0800

    feat: load exist dataset.json automatically

--- datasets/MATH.py
+++ datasets/MATH.py
@@ -14,24 +14,29 @@ class MATHDataset(Dataset):
     def __init__(self, basedir, dataset_type='train', sample_per_categorie_num=10, problem_types=None):
         self.data = []
 
-        for problem_type in os.listdir(f'{basedir}/{dataset_type}/'):
-            if ( problem_types == None ) or ( problem_type in problem_types ):
-                for problem_json in os.listdir(f'{basedir}/{dataset_type}/{problem_type}'):
-                    problem = load_json(f'{basedir}/{dataset_type}/{problem_type}/{problem_json}')
-                    answer = extract_answer(problem['solution'])
+        if not os.path.isfile('./dataset.json'):
+            print("Create dataset and save it to ./dataset.json")
+            for problem_type in os.listdir(f'{basedir}/{dataset_type}/'):
+                if ( problem_types == None ) or ( problem_type in problem_types ):
+                    for problem_json in os.listdir(f'{basedir}/{dataset_type}/{problem_type}'):
+                        problem = load_json(f'{basedir}/{dataset_type}/{problem_type}/{problem_json}')
+                        answer = extract_answer(problem['solution'])
 
-                    if isinstance(answer, int):
-                        self.data.append({
-                            'id': str(int(problem_json.replace('.json', ''))),
-                            'problem': str(problem['problem']),
-                            'level': str(problem['level'].replace('Level ', '')),
-                            'type': str(problem['type']),
-                            'answer': int(answer),
-                        })
-        random.shuffle(self.data)
-        self.sample_per_categories(sample_per_categorie_num)
+                        if isinstance(answer, int):
+                            self.data.append({
+                                'id': str(int(problem_json.replace('.json', ''))),
+                                'problem': str(problem['problem']),
+                                'level': str(problem['level'].replace('Level ', '')),
+                                'type': str(problem['type']),
+                                'answer': int(answer),
+                            })
+            random.shuffle(self.data)
+            self.sample_per_categories(sample_per_categorie_num)
 
-        dump_json(self.data, "./dataset.json")
+            dump_json(self.data, "./dataset.json")
+        else:
+            print("Load dataset from ./dataset.json")
+            self.data = load_json('./dataset.json')
 
     def __len__(self):
         return len(self.data)
